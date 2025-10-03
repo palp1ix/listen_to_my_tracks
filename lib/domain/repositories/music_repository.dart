87cf41e2bb4ebc5b808836f14a4a17275ff73601
palp1ix <arthach.2008@gmail.com>
2025-10-03 17:33:26 +0300

    feat(repository): Define MusicRepository interface for track search operations

---
+++ lib/domain/repositories/music_repository.dart
@@ -0,0 +1,17 @@
+// lib/domain/repositories/music_repository.dart
+
+import 'package:listen_to_my_tracks/core/error/exception.dart';
+import 'package:listen_to_my_tracks/core/utils/result.dart';
+import 'package:listen_to_my_tracks/domain/entities/track.dart';
+
+// Defines the contract for music-related data operations.
+abstract interface class MusicRepository {
+  // Searches for tracks based on a user-provided query.
+  //
+  // Returns a [Result] type, which encapsulates either a list of [TrackEntity]
+  // on success or an [AppException] on failure.
+  Future<Result<List<TrackEntity>, AppException>> searchTracks({
+    required String query,
+  });
+
+}
