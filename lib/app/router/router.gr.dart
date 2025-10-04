82d6ec4cf33841b4e01f49d358563023372ef39c
palp1ix <arthach.2008@gmail.com>
2025-10-05 02:12:32 +0300

    feat(router): Simplify SearchResultsRoute and integrate router in MainApp

--- lib/app/router/router.gr.dart
+++ lib/app/router/router.gr.dart
@@ -10,7 +10,6 @@
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
 import 'package:auto_route/auto_route.dart' as _i3;
-import 'package:flutter/material.dart' as _i4;
 import 'package:listen_to_my_tracks/features/home/view/home_screen.dart' as _i1;
 import 'package:listen_to_my_tracks/features/search/view/search_results_screen.dart'
     as _i2;
@@ -33,47 +32,16 @@ class HomeRoute extends _i3.PageRouteInfo<void> {
 
 /// generated route for
 /// [_i2.SearchResultsScreen]
-class SearchResultsRoute extends _i3.PageRouteInfo<SearchResultsRouteArgs> {
-  SearchResultsRoute({
-    _i4.Key? key,
-    required String query,
-    List<_i3.PageRouteInfo>? children,
-  }) : super(
-         SearchResultsRoute.name,
-         args: SearchResultsRouteArgs(key: key, query: query),
-         initialChildren: children,
-       );
+class SearchResultsRoute extends _i3.PageRouteInfo<void> {
+  const SearchResultsRoute({List<_i3.PageRouteInfo>? children})
+    : super(SearchResultsRoute.name, initialChildren: children);
 
   static const String name = 'SearchResultsRoute';
 
   static _i3.PageInfo page = _i3.PageInfo(
     name,
     builder: (data) {
-      final args = data.argsAs<SearchResultsRouteArgs>();
-      return _i2.SearchResultsScreen(key: args.key, query: args.query);
+      return const _i2.SearchResultsScreen();
     },
   );
-}
-
-class SearchResultsRouteArgs {
-  const SearchResultsRouteArgs({this.key, required this.query});
-
-  final _i4.Key? key;
-
-  final String query;
-
-  @override
-  String toString() {
-    return 'SearchResultsRouteArgs{key: $key, query: $query}';
-  }
-
-  @override
-  bool operator ==(Object other) {
-    if (identical(this, other)) return true;
-    if (other is! SearchResultsRouteArgs) return false;
-    return key == other.key && query == other.query;
-  }
-
-  @override
-  int get hashCode => key.hashCode ^ query.hashCode;
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:listen_to_my_tracks/app/resources/theme.dart';
+import 'package:listen_to_my_tracks/app/router/router.dart';
 import 'package:listen_to_my_tracks/features/home/view/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  final _router = AppRouter();
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       // The light theme is the default.
       theme: AppTheme.lightTheme,
       // The dark theme is used when the system requests it.
       darkTheme: AppTheme.darkTheme,
       // This makes the app theme react to system settings.
       themeMode: ThemeMode.system,
-      home: HomeScreen(),
+      // Router configuration
+      routerConfig: _router.config(),
     );
   }
 }
