2ae206fba8ab4ac1dbb4e5635c06677c5352ee38
palp1ix <arthach.2008@gmail.com>
2025-10-04 12:30:19 +0300

    feat(search): Implement SearchBloc with event and state management for track search functionality

---
+++ lib/features/search/bloc/search_bloc.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:listen_to_my_tracks/core/utils/result.dart';
+import 'package:listen_to_my_tracks/domain/entities/track.dart';
+import 'package:listen_to_my_tracks/domain/repositories/music_repository.dart';
+
+part 'search_event.dart';
+part 'search_state.dart';
+
+class SearchBloc extends Bloc<SearchEvent, SearchState> {
+  SearchBloc(this._musicRepository) : super(SearchInitial()) {
+    on<SearchRequested>(_onSearchRequested);
+  }
+
+  final MusicRepository _musicRepository;
+
+  FutureOr<void> _onSearchRequested(event, emit) async {
+    emit(SearchLoading());
+    final result = await _musicRepository.searchTracks(query: event.query);
+    switch (result) {
+      case Success(value: final tracks):
+        // An empty list is a valid success case, but the UI might need
+        // to show a specific "Not Found" message. The UI can decide this
+        // based on whether the list is empty.
+        emit(SearchSuccess(tracks));
+
+      // Pattern matching also extracts the exception for us.
+      case Failure(exception: final exception):
+        emit(SearchFailure(exception.message));
+    }
+  }
+}
---
+++ lib/features/search/bloc/search_state.dart
@@ -0,0 +1,34 @@
+part of 'search_bloc.dart';
+
+sealed class SearchState extends Equatable {
+  const SearchState();
+  
+  @override
+  List<Object> get props => [];
+}
+
+final class SearchInitial extends SearchState {}
+
+
+final class SearchLoading extends SearchState {}
+
+// Search failure state
+final class SearchFailure extends SearchState {
+  const SearchFailure(this.message);
+  
+  final String message;
+  
+  @override
+  List<Object> get props => [message];
+}
+
+// Search success state with results
+final class SearchSuccess extends SearchState {
+  const SearchSuccess(this.results);
+
+  final List<TrackEntity> results;
+  
+  @override
+  List<Object> get props => [results];
+}
+
